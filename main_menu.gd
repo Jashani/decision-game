79a2b6fab2671eaa85428317882753997d439801
Yasha <jashaniyt@gmail.com>
2025-04-18 10:47:07 +0300

    Implement main menu (#10)

---
+++ main_menu.gd
@@ -0,0 +1,20 @@
+extends Control
+
+func _ready() -> void:
+	_enable_keyboard_control()
+
+
+func _on_start_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://main.tscn")
+
+
+func _on_options_button_pressed() -> void:
+	push_error("Options not implemented.")
+
+
+func _on_quit_button_pressed() -> void:
+	get_tree().quit()
+
+
+func _enable_keyboard_control() -> void:
+	$VBoxContainer/StartButton.grab_focus()
