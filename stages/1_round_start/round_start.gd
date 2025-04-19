02a4b6a36f41a1c4edc28f340f3475445f05aed8
Yasha Rise <yrise@gitlab.com>
2025-04-19 12:26:30 +0300

    Fix paths and tidy up

--- stages/1_round_start/round_start.gd
+++ stages/1_round_start/round_start.gd
@@ -2,4 +2,4 @@ extends Control
 
 
 func _on_submit_button_pressed() -> void:
-	get_tree().change_scene_to_file("res://main.tscn")
+	get_tree().change_scene_to_file("res://stages/2_investigation/investigation.tscn")
--- stages/4_reveal/reveal_success.gd
+++ stages/4_reveal/reveal_success.gd
@@ -2,4 +2,4 @@ extends Control
 
 
 func _on_continue_button_pressed() -> void:
-	get_tree().change_scene_to_file("res://stages/main_menu/main_menu.tscn")
+	get_tree().change_scene_to_file("res://stages/0_main_menu/main_menu.tscn")
