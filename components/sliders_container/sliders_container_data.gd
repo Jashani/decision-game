4d28ebcc4bebc0cb7d3935864ced9be895e37c48
Yasha <jashaniyt@gmail.com>
2025-05-01 11:06:26 +0300

    Implement sliders container (#22)
    * Implement basic sliders container
    
    * Implement labels for sliders container

---
+++ components/sliders_container/sliders_container_data.gd
@@ -0,0 +1,9 @@
+class_name SlidersContainerData
+
+extends Resource
+
+@export var title: String = "Title"
+# Labels to display along the bottom of the sliders container
+@export var labels: PackedStringArray = []
+# Characters to have sliders for
+@export var characters: Array[CharacterData] = []
